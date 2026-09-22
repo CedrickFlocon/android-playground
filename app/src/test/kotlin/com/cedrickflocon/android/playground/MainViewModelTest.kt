e13f6a4f906e317ee468ca475e9827a4970b46f0
Cedrick Flocon <7161169+CedrickFlocon@users.noreply.github.com>
2026-09-22 10:09:26 +0200

    Remove test

--- app/src/test/kotlin/com/cedrickflocon/android/playground/MainViewModelTest.kt
+++ app/src/test/kotlin/com/cedrickflocon/android/playground/MainViewModelTest.kt
@@ -1,6 +1,5 @@
 package com.cedrickflocon.android.playground
 
-import app.cash.turbine.test
 import com.google.common.truth.Truth.assertThat
 import io.kotest.core.spec.style.DescribeSpec
 
@@ -9,10 +8,8 @@ class MainViewModelTest : DescribeSpec({
     describe("MainViewModel") {
         val viewModel = MainViewModel()
 
-        it("should expose initial uim state") {
-            viewModel.uim.test {
-                assertThat(awaitItem()).isEqualTo("Hello from ViewModel")
-            }
+        it("should have media items") {
+            assertThat(viewModel.mediaItems).isNotEmpty()
         }
     }
 })
